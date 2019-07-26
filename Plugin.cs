f171bba0c271c403c02115f877051cb886354dac
orgrinrt <ollipekka.toimela@gmail.com>
2019-07-26 16:30:02 +0300

    Complete overhaul of the plugin's folder structure

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,6 @@
 ﻿//#if TOOLS
+
+using System;
 using Godot;
 
 namespace Wayfarer
@@ -6,7 +8,17 @@ namespace Wayfarer
     [Tool]
     public class Plugin : EditorPlugin
     {
-        
+        public override void _EnterTree()
+        {
+            Script script = GD.Load<Script>("res://Addons/Wayfarer/WayfarerNode.cs");
+            Texture icon = GD.Load<Texture>("res://Addons/Wayfarer/Assets/Icons/wayfarer.png");
+            AddCustomType("WayfarerNode", "Node", script, icon);
+        }
+
+        public override void _ExitTree()
+        {
+            RemoveCustomType("WayfarerNode");
+        }
     }
 }
 //#endif
