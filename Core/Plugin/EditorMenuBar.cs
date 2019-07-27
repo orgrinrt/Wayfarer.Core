1c38e1cc3d02ba3a7d9e44ba6db592fb89d8097a
orgrinrt <ollipekka.toimela@gmail.com>
2019-07-27 16:02:20 +0300

    A lot of work towards making Wayfarer work

--- Core/Plugin/EditorMenuBar.cs
+++ Core/Plugin/EditorMenuBar.cs
@@ -1,40 +1,87 @@
 #if TOOLS
 
-using Godot;
 using System;
+using Godot;
 using Wayfarer.Utils.Attributes;
+using Wayfarer.Utils.Debug;
 using Wayfarer.Utils.Helpers;
 using Array = Godot.Collections.Array;
 
-[Tool]
-public class EditorMenuBar : Control
+namespace Wayfarer.Core.Plugin
 {
-    [Get("HBox/AddNodeMenu/Button")] private Button _addNodeButton;
-
-    private EditorInterface _editorInterface;
+    [Tool]
+    public class EditorMenuBar : Control
+    {
+        [Get("HBox/AddNodeMenu/Button")] private Button _addNodeButton;
     
-    public override void _Ready()
-    {
-        this.SetupWayfarer();
+        private Wayfarer.Core.Plugin.AddNodeDialog _addNodeDialog;
+        private EditorInterface _editorInterface;
 
-        _addNodeButton.Connect("button_up", this, nameof(OnButtonPressed));
-    }
-
-    private void OnButtonPressed()
-    {
-        EditorSelection selection = _editorInterface.GetSelection();
-        Array selectedNodes = selection.GetSelectedNodes();
-
-        foreach (Node node in selectedNodes)
+        public AddNodeDialog AddNodeDialog => _addNodeDialog;
+    
+        public override void _Ready()
         {
+            this.SetupWayfarer();
+
+            _addNodeButton.Connect("button_up", this, nameof(OnButtonPressed));
+        
+            PackedScene addNodeDialogScene = GD.Load<PackedScene>("res://Addons/Wayfarer/Assets/Scenes/Controls/AddNodeDialog.tscn");
+            Node node = addNodeDialogScene.Instance();
+            
+            try
+            {
+                Wayfarer.Core.Plugin.AddNodeDialog dialog = (Wayfarer.Core.Plugin.AddNodeDialog)addNodeDialogScene.Instance();
+            }
+            catch (Exception e)
+            {
+                Log.Editor(e.Message, true);
+            }
+            
+            //_addNodeDialog = (AddNodeDialog)addNodeDialogScene.Instance();
+            AddChild(node);
+            GD.Print(GetType());
+            GD.Print(addNodeDialogScene.Instance().GetType());
             GD.Print(node.Name);
+            GD.Print(node.GetType());
+        
+            try
+            {
+                _addNodeDialog = (AddNodeDialog)node;
+            }
+            catch (Exception e)
+            {
+                Log.Editor(e.Message, true);
+            }
+            GD.Print("Is not null: " + (_addNodeDialog != null));
+            //GD.Print(_addNodeDialog.Name);
+        
+            //_editorInterface.GetBaseControl().AddChild(_addNodeDialog);
+
         }
-        // open pop-up to select node to create
-    }
+
+        private void OnButtonPressed()
+        {
+            EditorSelection selection = _editorInterface.GetSelection();
+            Array selectedNodes = selection.GetSelectedNodes();
+
+            GD.Print(GetChildCount());
+            GD.Print(_addNodeDialog.Name);
+            //_addNodeDialog.Name = "Lol";
+            //GD.Print(_addNodeDialog.Name);
+            //_addNodeDialog.SetSelectedNodes(selectedNodes);
+            //_addNodeDialog.Popup_();
+        
+            foreach (Node node in selectedNodes)
+            {
+                // pass in the selectedNodes to the popup
+            }
+            // open pop-up to select node to create
+        }
     
-    public void SetEditorInterface(EditorInterface editor)
-    {
-        _editorInterface = editor;
+        public void SetEditorInterface(EditorInterface editor)
+        {
+            _editorInterface = editor;
+        }
     }
 }
 
--- Plugin.cs
+++ Plugin.cs
@@ -1,6 +1,11 @@
 ﻿#if TOOLS
 
+using System;
 using Godot;
+using Wayfarer.Core.Plugin;
+using Wayfarer.Utils.Debug;
+using Wayfarer.Utils.Helpers;
+using Texture = Godot.Texture;
 
 namespace Wayfarer
 {
@@ -9,8 +14,33 @@ namespace Wayfarer
     {
         private EditorMenuBar _toolbar;
         public EditorInterface EditorInterface => GetEditorInterface();
-        
+
+        public override bool Build()
+        {
+            if (!EditorInterface.IsPluginEnabled("Wayfarer")) // this should never happen, but just a sanity thing
+            {
+                Log.Editor("Solution was built, so removing the old EditorMenuBar...", true);
+                RemoveOldEditorMenubar();
+            }
+            
+            return base.Build();
+        }
+
         public override void _EnterTree()
+        {
+            AddCustomTypes();
+            AddCustomResources();
+            AddCustomControlsToEditor();
+        }
+
+        public override void _ExitTree()
+        {
+            RemoveCustomTypes();
+            RemoveCustomResources();
+            RemoveCustomControlsFromEditor();
+        }
+
+        private void AddCustomTypes()
         {
             Script wnScript = GD.Load<Script>("res://Addons/Wayfarer/Core/Nodes/WayfarerNode.cs");
             Texture wnIcon = GD.Load<Texture>("res://Addons/Wayfarer/Assets/Icons/wayfarer.png");
@@ -18,8 +48,24 @@ namespace Wayfarer
 
             Script managerScript = GD.Load<Script>("res://Addons/Wayfarer/Core/Systems/Managers/Manager.cs");
             Texture managerIcon = GD.Load<Texture>("res://Addons/Wayfarer/Assets/Icons/manager.png");
-            AddCustomType("Manager", "WayfarerNode", managerScript, managerIcon);
+            AddCustomType("Manager", "Node", managerScript, managerIcon);
+        }
 
+        private void AddCustomResources()
+        {
+            Script nodeScript = GD.Load<Script>("res://Addons/Wayfarer/Core/Plugin/Resources/WayfarerNodeData.cs");
+            Texture nodeIcon = GD.Load<Texture>("res://Addons/Wayfarer/Assets/Icons/resource.png");
+            AddCustomType("WayfarerNodeData", "Resource", nodeScript, nodeIcon);
+
+            Script nodeListScript = GD.Load<Script>("res://Addons/Wayfarer/Core/Plugin/Resources/WayfarerNodeList.cs");
+            Texture nodeListIcon = GD.Load<Texture>("res://Addons/Wayfarer/Assets/Icons/wayfarerNodes.png");
+            AddCustomType("WayfarerNodeList", "Resource", nodeListScript, nodeListIcon);
+        }
+
+        private void AddCustomControlsToEditor()
+        {
+            RemoveOldEditorMenubar();
+            
             PackedScene toolbarScene = GD.Load<PackedScene>("res://Addons/Wayfarer/Assets/Scenes/Controls/EditorMenuBar.tscn");
             _toolbar = (EditorMenuBar)toolbarScene.Instance();
             _toolbar.SetEditorInterface(GetEditorInterface());
@@ -27,11 +73,36 @@ namespace Wayfarer
             AddControlToContainer(CustomControlContainer.CanvasEditorMenu, _toolbar);
         }
 
-        public override void _ExitTree()
+        private void RemoveOldEditorMenubar()
+        {
+            Node[] editorNodes = EditorInterface.GetBaseControl().GetChildrenRecursive();
+
+            foreach (Node node in editorNodes)
+            {
+                if (node is EditorMenuBar)
+                {
+                    RemoveControlFromContainer(CustomControlContainer.CanvasEditorMenu, node as Control);
+                    node.QueueFree();
+                    Log.Editor("Removed old EditorMenuBar", true);
+                    return;
+                }
+            }
+        }
+
+        private void RemoveCustomTypes()
         {
             RemoveCustomType("WayfarerNode");
             RemoveCustomType("Manager");
-            
+        }
+
+        private void RemoveCustomResources()
+        {
+            RemoveCustomType("WayfarerNodeData");
+            RemoveCustomType("WayfarerNodeList");
+        }
+
+        private void RemoveCustomControlsFromEditor()
+        {
             RemoveControlFromContainer(CustomControlContainer.CanvasEditorMenu, _toolbar);
         }
     }
