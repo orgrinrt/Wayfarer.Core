222ecf1182796965865f790187459dc2d1187f9c
orgrinrt <ollipekka.toimela@gmail.com>
2019-07-28 18:04:37 +0300

    Renamed WayfarerNodeDatabase to make it more understandable, minor fixes on Resource structures but nothing settled, future changes coming shortly

--- Core/Plugin/Resources/WayfarerNodeData.cs
+++ Core/Plugin/Resources/WayfarerNodeData.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using Godot;
 using Godot.Collections;
+using Wayfarer.Utils.Debug;
 
 namespace Wayfarer.Core.Plugin.Resources
 {
@@ -8,13 +9,72 @@ namespace Wayfarer.Core.Plugin.Resources
     {
         [Export()] private string _nodeName;
         [Export()] private string _parentName;
-        [Export()] private bool _isAScene;
+
+        [Export()] private bool _isScene;
+        [Export()] private Script _script;
         [Export()] private PackedScene _scene;
+        
+        [Export()] private string _description;
         [Export()] private Texture _icon;
 
         public string NodeName => _nodeName;
         public string ParentName => _parentName;
         public PackedScene Scene => _scene;
         public Texture Icon => _icon;
+
+        public override void _Init()
+        {
+            base._Init();
+            
+            Log.Wayfarer("NodeData " + _nodeName + " was initialized", true);
+        }
+
+        public override Array _GetPropertyList()
+        {
+            Array source = base._GetPropertyList();
+
+            foreach (Dictionary property in source)
+            {
+                if (property.TryGetValue("name", out object value))
+                {
+                    if (value is string s)
+                    {
+                        if (s == "_script")
+                        {
+                            if (_isScene)
+                            {
+                                Log.Wayfarer("Removed property " + s, true);
+                                source.Remove(property);
+                            } 
+                        }
+                        else if (s == "_scene")
+                        {
+                            if (!_isScene)
+                            {
+                                Log.Wayfarer("Removed property " + s, true);
+                                source.Remove(property);
+                            }
+                        }
+                        else
+                        {
+                            Log.Wayfarer("There was no property named _script or _scene (current: " + s, true);
+                        }
+                    }
+                }
+                else
+                {
+                    Log.Wayfarer("A property didn't have a name", true);
+                }
+            }
+
+            foreach (Dictionary property in source)
+            {
+                if (property.TryGetValue("name", out object value))
+                {
+                    Log.Wayfarer("Property: " + value, true);
+                }
+            }
+            return source;
+        }
     }
 }
---
+++ Core/Plugin/Resources/WayfarerNodeDatabase.cs
@@ -0,0 +1,12 @@
+﻿using Godot;
+using Godot.Collections;
+
+namespace Wayfarer.Core.Plugin.Resources
+{
+    public class WayfarerNodeDatabase : Resource
+    {
+        [Export()] private Array<WayfarerNodeData> _all;
+
+        public Array<WayfarerNodeData> All => _all;
+    }
+}
