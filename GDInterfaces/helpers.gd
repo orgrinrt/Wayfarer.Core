afb29a1c62aa22f06008c63ad57bb54009c62b36
orgrinrt <ollipekka.toimela@gmail.com>
2019-07-31 18:24:50 +0300

    Renamed cs_log variable -> cs_bridge (this was legacy definition)

--- GDInterfaces/helpers.gd
+++ GDInterfaces/helpers.gd
@@ -2,8 +2,8 @@ tool
 extends Node
 
 static func get_children_recursive(var node : Node) -> Array:
-	var cs_log = load("res://Addons/Wayfarer/Core/Gd.cs");
-	var cs_instance = cs_log.new();
+	var cs_bridge = load("res://Addons/Wayfarer/Core/Gd.cs");
+	var cs_instance = cs_bridge.new();
 	var children = cs_instance.call("GetChildrenRecursive", node);
 	cs_instance.queue_free();
 	return children;
