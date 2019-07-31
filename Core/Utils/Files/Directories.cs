e2b950f168d4dba353813047d97b2d089009ba13
orgrinrt <ollipekka.toimela@gmail.com>
2019-07-31 18:23:19 +0300

    Added in Pebble related paths to Paths as well as added in a static Directories helper

---
+++ Core/Utils/Files/Directories.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using Wayfarer.Core.Utils.Debug;
+
+namespace Wayfarer.Core.Utils.Files
+{
+    public static class Directories
+    {
+        public static DirectoryInfo PebblesRoot => GetWayfarerPebblesRootDirectory();
+        public static DirectoryInfo WayfarerLogRoot => GetWayfarerLogRootDirectory();
+        
+        static Directories()
+        {
+            try
+            {
+                Initialize();
+            }
+            catch (Exception e)
+            {
+                Log.Error("Tried to initialize Directories() but couldn't", e, true);
+            }
+        }
+
+        public static void Initialize()
+        {/*
+            GetWayfarerLogRootDirectory();
+            GetWayfarerPebblesRootDirectory();*/
+        }
+
+        private static DirectoryInfo GetWayfarerPebblesRootDirectory()
+        {
+            return Directory.CreateDirectory(Paths.PebblesPath);
+        }
+
+        private static DirectoryInfo GetWayfarerLogRootDirectory()
+        {
+            return Directory.CreateDirectory(Paths.WayfarerLogPath);
+        }
+    }
+}
